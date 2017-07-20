70ba6576363e255fef3b281e5449f7d47b06c154
Timothy Batty <tbatty7@gmail.com>
2017-07-19 23:05:16 -0400

    put API responses in html tags

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
   $.get('https://www.reddit.com/r/aww.json', function(response) {
  for(var i=0; i<10; i++){
-   console.log(response.data.children[i].data.author);}
+   console.log(response.data.children[i].data.author);
+   $('body').append('<p> Title: ' + response.data.children[i].data.title + '</p>');
+   $('body').append('<p> Author: ' + response.data.children[i].data.author + '</p>');
+   $('body').append('<img src="' + response.data.children[i].data.thumbnail + '" alt=""></img>');
+}
 });
 $.get('https://www.reddit.com/r/aww.json', function(response) {
 for(var i=0; i<10; i++){
- console.log(response.data.children[i].data.thumbnail);}
+ console.log(response.data.children[i].data.thumbnail);
+}
 });
 $.get('https://www.reddit.com/r/aww.json', function(response) {
 for(var i=0; i<10; i++){
- console.log(response.data.children[i].data.title);}
+ console.log(response.data.children[i].data.title);
+}
 });
+
 });
