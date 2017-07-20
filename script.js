1187bc71f30f916e36f0db4361857eb7e9eea8c9
Brittany <brithamilton1@gmail.com>
2017-07-20 19:34:45 -0400

    javascript update

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 $(document).ready(function() {
   $.get('https://www.reddit.com/r/aww.json', function(response) {
- for(var i=0; i<10; i++){
-   console.log(response.data.children[i].data.author);
-   $('body').append('<p> Title: <a href ="' + response.data.children[i].data.url + '">'  + response.data.children[i].data.title + '</a></p>');
-   $('body').append('<p> Author: ' + response.data.children[i].data.author + '</p>');
-   $('body').append('<img src="' + response.data.children[i].data.thumbnail + '" alt=""></img>');
-}
+   for(var i=0; i<10; i++){
+     var div = '<div>';
+     div += '<p> Title: <a href ="' + response.data.children[i].data.url + '">'  + response.data.children[i].data.title + '</a></p>';
+     div += '<p> Author: ' + response.data.children[i].data.author + '</p>'
+     div += '<img src="' + response.data.children[i].data.thumbnail + '" alt=""></img>'
+     div += '</div>'
+     $('body').append(div);
+  }
 });
-// $.get('https://www.reddit.com/r/aww.json', function(response) {
-// for(var i=0; i<10; i++){
-//  console.log(response.data.children[i].data.thumbnail);
-// }
-// });
-// $.get('https://www.reddit.com/r/aww.json', function(response) {
-// for(var i=0; i<10; i++){
-//  console.log(response.data.children[i].data.title);
-// }
-// });
 
 });
