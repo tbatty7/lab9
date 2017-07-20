80db61e766ab3285acb456dbf65dda609b975285
Brittany <brithamilton1@gmail.com>
2017-07-19 21:23:07 -0400

    js added

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-$.get('https://www.reddit.com/r/aww/.json', function(responseBody){
-console.log(responseBody);
-
-})
+  $.get('https://www.reddit.com/r/aww.json', function(response) {
+ for(var i=0; i<10; i++){
+   console.log(response.data.children[i].data.author);}
+});
+$.get('https://www.reddit.com/r/aww.json', function(response) {
+for(var i=0; i<10; i++){
+ console.log(response.data.children[i].data.thumbnail);}
+});
+$.get('https://www.reddit.com/r/aww.json', function(response) {
+for(var i=0; i<10; i++){
+ console.log(response.data.children[i].data.title);}
+});
 });
