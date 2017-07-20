a01a700ce2cfcec2b28c76fcada62a916247c4bd
Demonshae Glover <dshaeglover@gmail.com>
2017-07-20 19:28:43 -0400

    made changes to html and js file

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
   $.get('https://www.reddit.com/r/aww.json', function(response) {
  for(var i=0; i<10; i++){
    console.log(response.data.children[i].data.author);
-   $('body').append('<p> Title: ' + response.data.children[i].data.title + '</p>');
+   $('body').append('<p> Title: <a href ="' + response.data.children[i].data.url + '">'  + response.data.children[i].data.title + '</a></p>');
    $('body').append('<p> Author: ' + response.data.children[i].data.author + '</p>');
    $('body').append('<img src="' + response.data.children[i].data.thumbnail + '" alt=""></img>');
 }
 });
-$.get('https://www.reddit.com/r/aww.json', function(response) {
-for(var i=0; i<10; i++){
- console.log(response.data.children[i].data.thumbnail);
-}
-});
-$.get('https://www.reddit.com/r/aww.json', function(response) {
-for(var i=0; i<10; i++){
- console.log(response.data.children[i].data.title);
-}
-});
+// $.get('https://www.reddit.com/r/aww.json', function(response) {
+// for(var i=0; i<10; i++){
+//  console.log(response.data.children[i].data.thumbnail);
+// }
+// });
+// $.get('https://www.reddit.com/r/aww.json', function(response) {
+// for(var i=0; i<10; i++){
+//  console.log(response.data.children[i].data.title);
+// }
+// });
 
 });
