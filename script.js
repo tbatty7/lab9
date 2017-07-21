c12e0e5d2dc6dd9ea5106ef98edfbb1085fa5b49
Timothy Batty <tbatty7@gmail.com>
2017-07-20 20:58:26 -0400

    added stylings in CSS

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   $.get('https://www.reddit.com/r/aww.json', function(response) {
    for(var i=0; i<10; i++){
      var div = '<div>';
-     div += '<p> Title: <a target="_blank" href ="' + response.data.children[i].data.url + '">'  + response.data.children[i].data.title + '</a></p>';
+     div += '<p> Title: <a target="_blank" href ="' + response.data.children[i].data.url + '">'  + 
+     response.data.children[i].data.title + '</a></p>';
      div += '<p> Author: ' + response.data.children[i].data.author + '</p>'
      div += '<img src="' + response.data.children[i].data.thumbnail + '" alt=""></img>'
      div += '</div>'
